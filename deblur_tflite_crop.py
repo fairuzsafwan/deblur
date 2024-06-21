43bd23ed92db7da7cca30ffc8114bdbe3e8d0792
b-knd <angjingru@gmail.com>
2024-06-21 09:22:55 +0800

    Added deblur main code using tflite model (deblur_tflite.py) and tflite model + cropping (deblur_tflite_crop.py)

---
+++ deblur_tflite_crop.py
@@ -0,0 +1,311 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import time
+import argparse
+
+import model as mdl
+
+# Define a custom dataset class
+class CustomDataset(tf.keras.utils.Sequence):
+    def __init__(self, image_path_folder, batch_size=32, input_shape=(256, 256), shuffle=True):
+        self.image_root_path = image_path_folder
+        self.batch_size = batch_size
+        self.input_shape = input_shape
+        self.shuffle = shuffle
+        self.image_gt = os.listdir(os.path.join(image_path_folder, "sharp"))
+        self.image_train = os.listdir(os.path.join(image_path_folder, "motion_blurred_v2"))
+
+    def __len__(self):
+        return int(np.ceil(len(self.image_train) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_image_train = self.image_train[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_image_gt = self.image_gt[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        train_images = []
+        gt_images = []
+
+        for img_train, img_gt in zip(batch_image_train, batch_image_gt):
+            train_image_path = os.path.join(self.image_root_path, "motion_blurred_v2", img_train)
+            gt_image_path = os.path.join(self.image_root_path, "sharp", img_gt)
+
+            train_image = read_image(train_image_path, img_size=self.input_shape)
+            gt_image = read_image(gt_image_path, img_size=self.input_shape)
+
+            # cv2.imshow("Train", train_image)
+            # cv2.imshow("GT", gt_image)
+            # cv2.waitKey(0)
+
+            train_images.append(train_image)
+            gt_images.append(gt_image)
+
+        return np.array(train_images), np.array(gt_images)
+
+class DoubleConv(tf.keras.Model):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.double_conv = tf.keras.Sequential([
+            layers.Conv2D(out_channels, kernel_size=3, padding='same'),
+            layers.ReLU(),
+            layers.Conv2D(out_channels, kernel_size=3, padding='same'),
+            layers.ReLU()
+        ])
+
+    def call(self, x):
+        return self.double_conv(x)
+
+class Down(tf.keras.Model):
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.maxpool_conv = tf.keras.Sequential([
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            DoubleConv(in_channels, out_channels)
+        ])
+
+    def call(self, x):
+        return self.maxpool_conv(x)
+
+class Up(tf.keras.Model):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        self.bilinear = bilinear
+        if not bilinear:
+            self.up = layers.Conv2DTranspose(in_channels // 2, kernel_size=2, strides=2, padding='same')
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def call(self, x1, x2):
+        if self.bilinear:
+            x1 = layers.UpSampling2D(size=(2, 2), interpolation='bilinear')(x1)
+        else:
+            x1 = self.up(x1)
+        diffY = x2.shape[1] - x1.shape[1]
+        diffX = x2.shape[2] - x1.shape[2]
+        x1 = layers.ZeroPadding2D(padding=((diffX // 2, diffX - diffX // 2), (diffY // 2, diffY - diffY // 2)))(x1)
+        x = tf.concat([x2, x1], axis=-1)
+        return self.conv(x)
+
+class UNet(tf.keras.Model):
+    def __init__(self, n_channels, n_classes):
+        super(UNet, self).__init__()
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256)
+        self.up2 = Up(512, 128)
+        self.up3 = Up(256, 64)
+        self.up4 = Up(128, 64)
+        self.outc = layers.Conv2D(n_classes, kernel_size=1)
+
+    def call(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        results = self.outc(x)
+        return results
+
+def read_image(image_path, img_size=(512, 512)):
+    img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, img_size)  # Resize images to a fixed size
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    img = img / 255.0  # Normalize the images
+    return img
+
+def read_inference_image(image_path, img_size=(256, 256), normalize=False):
+    try:
+        img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        if img is None:
+            raise ValueError(f"Error: Failed to load image at {image_path}")
+
+        # Calculate aspect ratio
+        h, w, _ = img.shape
+        target_h, target_w = img_size
+
+        if h > w:
+            # Crop height
+            start_h = (h - w) // 2
+            img = img[start_h:start_h + w, :, :]
+        else:
+            # Crop width
+            start_w = (w - h) // 2
+            img = img[:, start_w:start_w + h, :]
+
+        # Resize to target size
+        img = cv2.resize(img, (target_w, target_h))
+
+        if normalize:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+            img = img.astype(np.float32) / 255.0  # Normalize the images
+
+        return img
+    except Exception as e:
+        print(f"Exception in reading image {image_path}: {e}")
+        return None
+
+def processDataset(dataset_path, batch_size, input_shape):
+    train_dataset = CustomDataset(image_path_folder=dataset_path, batch_size=batch_size, input_shape=input_shape)
+    return train_dataset
+
+def trainModel(num_epochs=10, learning_rate=0.001, train_loader=None, model_path="./"):
+    print("------------- Training Start -------------")
+    model = UNet(n_channels=3, n_classes=3)
+    # model = mdl.FPN(n_channels=3, n_classes=3)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    criterion = tf.keras.losses.MeanSquaredError()
+
+    @tf.function
+    def train_step(train_img, gt_img):
+        with tf.GradientTape() as tape:
+            pred_img = model(train_img)
+            loss = criterion(gt_img, pred_img)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        return loss
+
+    start_t = time.time()
+
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        for batch_images, batch_gt_images in train_loader:
+            loss = train_step(batch_images, batch_gt_images)
+            total_loss += loss
+
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {total_loss}')
+
+        #model.save_weights(model_path_name)
+        model.save(os.path.join(model_path, str(epoch+1)))
+
+        #Clears sessions for memory leak issues
+        tf.keras.backend.clear_session()
+    
+    end_t = time.time() - start_t
+    print(f"Time: {int(end_t // 60)} mins {int(end_t % 60)} secs")
+
+    print("------------- Training completed -------------")
+
+def convertModel(model_path):
+    print("------------- Conversion start -------------")
+    # Load the model
+    model = tf.keras.models.load_model(model_path)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+
+    # Save the TensorFlow Lite model
+    with open(os.path.join(model_path, os.path.basename(model_path)) + ".tflite", "wb") as f:
+        f.write(tflite_model)
+    print("------------- Conversion completed -------------")
+
+def inference(model_path, img_path, output_path, resized_original_imgpath, img_size):
+    print("------------- Inference start -------------")
+
+    try:
+        # Initialize TensorFlow Lite interpreter
+        interpreter = tf.lite.Interpreter(model_path=os.path.join(model_path, "200.tflite"))
+        interpreter.allocate_tensors()
+
+        # Get input and output details
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+    except Exception as e:
+        print(f"Error loading model from {model_path}: {e}")
+        return
+
+    # Prepare output directories
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        
+    if not os.path.exists(resized_original_imgpath):
+        os.makedirs(resized_original_imgpath)
+
+    # Iterate through all images in the input directory
+    img_paths = [os.path.join(img_path, path) for path in os.listdir(img_path)]
+
+    for img_path in img_paths:
+        try:
+            # Load and preprocess the input image
+            input_img = read_inference_image(img_path, img_size=img_size, normalize=True)
+            if input_img is None:
+                continue
+
+            # Set input tensor
+            input_img = np.expand_dims(input_img, axis=0).astype(np.float32)
+            interpreter.set_tensor(input_details[0]['index'], input_img)
+
+            # Perform inference
+            interpreter.invoke()
+
+            # Get output tensor
+            output_data = interpreter.get_tensor(output_details[0]['index'])
+            output_img = np.squeeze(output_data)
+
+            # Post-process the output image (denormalize, clip, convert to uint8)
+            output_img = output_img * 255.0
+            output_img = np.clip(output_img, 0, 255).astype(np.uint8)
+
+            # Save output image
+            output_image_path = os.path.join(output_path, os.path.basename(img_path))
+            cv2.imwrite(output_image_path, cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR))
+            print(f"Output image saved at {output_image_path}")
+        except Exception as e:
+            print(f"Error processing image {img_path}: {e}")
+            continue
+
+    print("------------- Inference completed -------------")
+
+
+if __name__ == "__main__":
+    tf.keras.backend.clear_session()
+
+    parser = argparse.ArgumentParser(description="Train deblurring model | Convert model to Tensorflow Lite")
+    parser.add_argument("--train", "-t", action="store_true", help="train deblurring model")
+    parser.add_argument("--infer", "-i", action="store_true", help="inference")
+    parser.add_argument("--convert", "-c", action="store_true", help="convert pytorch model to ONNX and then to Tensorflow Lite")
+    parser.add_argument("--path", "-p", default=False, help="path to store saved models")
+    args = parser.parse_args()
+
+    # parameters
+    num_epochs = 200
+    learning_rate = 0.001
+    batch_size = 18 #32
+    dataset_path = "blur_dataset"
+    model_path = "256_e200_unet_b18_tf/200"
+    output_path = "result_tflite_crop"
+    inference_path = "test_image"
+    resized_original_imgpath = "resized_test_image"
+    train_loader = None
+    img_size = (256, 256) #(256, 256) #(512, 512) (224,224)
+    
+    #Create directory to save model
+    if args.path:
+        model_path = args.path
+    
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+
+    print("Model Path: ", model_path)
+
+    # train model
+    if args.train:     
+        #process dataset
+        train_loader = processDataset(dataset_path, batch_size, img_size)
+
+        # Train model
+        trainModel(num_epochs, learning_rate, train_loader, model_path)
+
+    if args.convert:
+        convertModel(model_path)
+
+    if args.infer:
+        inference(model_path, inference_path, output_path, resized_original_imgpath, img_size)
